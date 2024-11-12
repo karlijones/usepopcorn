ae70215a5e838a83d8598977e3b5b647f6b614ba
karlijones <karlijones1984@gmail.com>
2024-11-11 20:35:13 -0600

    separate function for movie list

--- src/App.js
+++ src/App.js
@@ -111,7 +111,6 @@ function Main() {
 }
 
 function ListBox() {
-  const [movies, setMovies] = useState(tempMovieData);
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -123,7 +122,16 @@ function ListBox() {
               {isOpen1 ? "–" : "+"}
             </button>
             {isOpen1 && (
-              <ul className="list">
+              <MovieList />
+            )}
+          </div>
+  );
+}
+
+function MovieList() {
+  const [movies, setMovies] = useState(tempMovieData);
+  return (
+    <ul className="list">
                 {movies?.map((movie) => (
                   <li key={movie.imdbID}>
                     <img src={movie.Poster} alt={`${movie.Title} poster`} />
@@ -137,8 +145,6 @@ function ListBox() {
                   </li>
                 ))}
               </ul>
-            )}
-          </div>
   );
 }
 
