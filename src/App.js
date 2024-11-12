ae48192d1e6a83e15ac129bacbc13e5a2afc864f
karlijones <karlijones1984@gmail.com>
2024-11-11 20:42:44 -0600

    separate movie function

--- src/App.js
+++ src/App.js
@@ -121,11 +121,10 @@ function ListBox() {
             >
               {isOpen1 ? "–" : "+"}
             </button>
-            {isOpen1 && (
-              <MovieList />
-            )}
+
+            {isOpen1 && <MovieList />}
           </div>
-  );
+    );
 }
 
 function MovieList() {
@@ -133,18 +132,24 @@ function MovieList() {
   return (
     <ul className="list">
                 {movies?.map((movie) => (
-                  <li key={movie.imdbID}>
-                    <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                    <h3>{movie.Title}</h3>
-                    <div>
-                      <p>
-                        <span>🗓</span>
-                        <span>{movie.Year}</span>
-                      </p>
-                    </div>
-                  </li>
+                 <Movie movie={movie} key={movie.imdbID} /> 
                 ))}
               </ul>
+  );
+}
+
+function Movie({ movie }) {
+  return (
+          <li >
+              <img src={movie.Poster} alt={`${movie.Title} poster`} />
+                <h3>{movie.Title}</h3>
+                  <div>
+                    <p>
+                      <span>🗓</span>
+                      <span>{movie.Year}</span>
+                    </p>
+                  </div>
+          </li>
   );
 }
 
