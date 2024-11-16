ff83765adaf003c5ac59ec0caefcd05de6393e96
karlijones <karlijones1984@gmail.com>
2024-11-16 13:28:38 -0600

    inline CSS styling

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,6 +1,12 @@
 export default function StarRating() {
+  const containerStyle={
+    display: "flex",
+    alignItems: "center",
+    gap: "16px",
+  };  
+    
     return (
-        <div>
+        <div style={containerStyle}>
             <div>
                 {Array.from({length: 5}, (_, i) => (
                     <span>S{i+1}</span>))}
