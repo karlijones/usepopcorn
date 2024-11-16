bb5260e73e4bf1182e1587e5de747a42b0b2db06
karlijones <karlijones1984@gmail.com>
2024-11-15 21:32:54 -0600

    Replacing children props with element

--- src/App.js
+++ src/App.js
@@ -61,15 +61,24 @@ export default function App() {
         <NumResults movies={movies} /> 
       </NavBar>
 
-      <Main>
-        <Box element={<MovieList movies={movies} />}/>
+       <Main>   
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList watched={watched}/>
+            </>
+          }
+      />
+
+
         {/* <Box>
-        
+          <MovieList movies={movies} />
         </Box>
 
         <Box>
           <WatchedSummary watched={watched}/>
-            <MovieList movies={movies} />
           <WatchedMoviesList watched={watched}/>
         </Box> */}
       </Main>
