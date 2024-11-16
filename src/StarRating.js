1212b427171ed9640e31975285a4abc19a15ce18
karlijones <karlijones1984@gmail.com>
2024-11-16 13:36:15 -0600

    styles for star container and text

--- src/StarRating.js
+++ src/StarRating.js
@@ -2,16 +2,26 @@ const containerStyle={
     display: "flex",
     alignItems: "center",
     gap: "16px",
-  }; 
+}; 
+
+const starContainerStyle = {
+    display: "flex", 
+    gap: "4px",
+};
+
+const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+};
 
 export default function StarRating() {
     return (
         <div style={containerStyle}>
-            <div>
+            <div style={starContainerStyle}>
                 {Array.from({length: 5}, (_, i) => (
                     <span>S{i+1}</span>))}
             </div>
-            <p>10</p>
+            <p style={textStyle}>10</p>
         </div>
     );
 }
