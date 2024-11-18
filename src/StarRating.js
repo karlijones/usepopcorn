ef8fb7aadb0a39391fe43891aaf0c4844301dfdd
karlijones <karlijones1984@gmail.com>
2024-11-17 20:56:25 -0600

    type checking with PropTypes

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
 
 const containerStyle={
     display: "flex",
@@ -10,7 +11,16 @@ const starContainerStyle = {
     display: "flex", 
 };
 
-
+//when using propTypes the "p" must be lowercase. See below
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    defaultRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number, 
+    messages: PropTypes.array,
+    className: PropTypes.string,
+    onSetRating: PropTypes.func,
+};
 
 export default function StarRating({ 
     maxRating = 5, 
