1d24da8c8ab684c593347fed90fc442ed05e10ab
karlijones <karlijones1984@gmail.com>
2024-11-24 11:31:21 -0600

    cleaning up details

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
 
-const containerStyle={
+const containerStyle= {
     display: "flex",
     alignItems: "center",
     gap: "16px",
@@ -43,7 +43,7 @@ export default function StarRating({
         lineHeight: "1",
         margin: "0",
         color,
-        fontSize: `${size / 1.5}px`
+        fontSize: `${size / 1.5}px`,
     };
 
     return (
@@ -79,7 +79,10 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
     }
 
     return (
-        <span role="button" style={starStyle} onClick={onRate} 
+        <span 
+        role="button" 
+        style={starStyle} 
+        onClick={onRate} 
         onMouseEnter={onHoverIn}
         onMouseLeave={onHoverOut}
         >
@@ -107,9 +110,10 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
             strokeWidth="{2}"
             d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
         />
-            </svg>)}
-        </span>
-    )
+        </svg>
+      )}
+    </span>
+  );
 }
 /*
 FULL STAR
