20156fd9605d5df6860ef0c046860c57495207d5
karlijones <karlijones1984@gmail.com>
2024-11-24 11:15:33 -0600

    API key

--- src/App.js
+++ src/App.js
@@ -50,9 +50,16 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = "3cec4470";
+
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
+
+  fetch(`http://www.omdbapi.com/?apikey=${KEY}&
+    s=interstellar`)
+    .then(res => res.json())
+    .then(data =>console.log(data));
 
   return (
     <>
