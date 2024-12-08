681fe093072d2e9704f8a24ac7243b14400fc8f8
karlijones <karlijones1984@gmail.com>
2024-12-08 11:57:00 -0600

    cleanup function for title

--- src/App.js
+++ src/App.js
@@ -359,6 +359,10 @@ useEffect(
   function() {
     if(!title) return;
     document.title = `Movie | ${title}`;
+
+    return function() {
+      document.title = "usePopcorn";
+    }
   }, 
   [title]
 );
