aa2dac97f35b34c77b01abc3ebebe6f4e174a62c
karlijones <karlijones1984@gmail.com>
2024-12-08 13:53:34 -0600

    cleanup function to prevent keypress event from happening more often than necessary

--- src/App.js
+++ src/App.js
@@ -353,12 +353,17 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}) {
   }
 
   useEffect(function(){
-    document.addEventListener("keydown", function(e) {
+    function callback (e) {
       if(e.code === "Escape") {
         onCloseMovie();
         console.log("CLOSING");
       }
-    });
+    }
+    document.addEventListener("keydown", callback);
+
+    return function() {
+      document.removeEventListener("keydown", callback);
+    }
   }, [onCloseMovie]);
 
   useEffect(
