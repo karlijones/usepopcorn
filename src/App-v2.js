1a6dd79c1e3a55f07ef40d5a482cee9cc2e946bc
karlijones <karlijones1984@gmail.com>
2025-01-05 12:49:33 -0600

    calling useLocalStorageState

--- src/App-v2.js
+++ src/App-v2.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 import { useMovies } from "./useMovies";
+import { useLocalStorageState } from "./useLocalStorageState";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -12,11 +13,13 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
   const { movies, isLoading, error } = useMovies(query);
 
+  const [watched, setWatched] = useLocalStorageState([])
+
   /*const [watched, setWatched] = useState([]);*/
-  const [watched, setWatched] = useState(function () {
+  /*const [watched, setWatched] = useState(function () {
     const storedValue = localStorage.getItem("watched");
     return JSON.parse(storedValue);
-  });
+  });*/
   
 
 function handleSelectMovie(id) {
