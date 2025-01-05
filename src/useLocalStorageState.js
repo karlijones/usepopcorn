21068010193db8c471083e840a02e5c2e9584b3c
karlijones <karlijones1984@gmail.com>
2025-01-05 12:51:43 -0600

    import useState

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,1 +1,8 @@
-export function useLocalStorageState() {}
+import { useState } from "react";
+
+export function useLocalStorageState(initialState) {
+    const [watched, setWatched] = useState(function () {
+        const storedValue = localStorage.getItem("watched");
+        return JSON.parse(storedValue);
+      });
+}
