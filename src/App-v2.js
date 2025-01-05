2586ddc856806070cd4e47dced4763de84bf854b
karlijones <karlijones1984@gmail.com>
2025-01-04 20:48:38 -0600

    destructuring data

--- src/App-v2.js
+++ src/App-v2.js
@@ -11,7 +11,7 @@ export default function App() {
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
 
-  useMovies(query)
+  const { movies, isLoading, error } = useMovies(query)
 
   /*const [watched, setWatched] = useState([]);*/
   const [watched, setWatched] = useState(function () {
