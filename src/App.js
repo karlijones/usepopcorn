13d17ea0ee5d4ea777a5374b16807eaf509d3449
karlijones <karlijones1984@gmail.com>
2025-01-06 12:22:51 -0600

    debugging

--- src/App.js
+++ src/App.js
@@ -1,52 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
-
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+import "./App.css";
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -56,11 +10,17 @@ const KEY = "3cec4470";
 export default function App() {
   const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
+
+
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [selectedId, setSelectedId] = useState(null);
   
+  const [watched, setWatched] = useState([]);
+  const [watched, setWatched] = useState(function () {
+    const watched = localStorage.getItem("watched");
+    return watched ? JSON.parse(watched) : [];
+  });
 
   //Dependency Array Included - will be executed last
   /*useEffect(function() {
@@ -100,6 +60,11 @@ function handleDeleteWatched(id) {
   setWatched(watched => watched.filter(movie=>movie.imdbID !== id));
 }
 
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    }, [watched]
+  );
 
   useEffect(
     function() {
@@ -136,7 +101,7 @@ function handleDeleteWatched(id) {
 
           if (query.length < 3) {
             setMovies([]);
-            setError('');
+            setError("");
             return;
           }
 
@@ -159,11 +124,11 @@ function handleDeleteWatched(id) {
 
        <Main>   
         <Box>
-          {//isLoading ? <Loader /> : <MovieList movies={movies} />
-          }
+          {/*isLoading ? <Loader /> : <MovieList movies={movies} />
+          } */}
           {isLoading && <Loader />}
-          {!isLoading && !error && <MovieList movies=
-          {movies} onSelectMovie={handleSelectMovie}/>}
+          {!isLoading && !error && (<MovieList movies=
+          {movies} onSelectMovie={handleSelectMovie}/>)}
           {error && <ErrorMessage message={error} />}
         </Box>
 
@@ -221,7 +186,56 @@ function Logo() {
   );
 }
 
-function Search(query, setQuery) {
+
+function Search({ query, setQuery }) {
+
+  const inputEl = useRef(null);
+
+
+
+  useEffect(
+
+    function () {
+
+      function callback(e) {
+
+        if (document.activeElement === inputEl.current) return;
+
+
+
+        if (e.code === "Enter") {
+
+          inputEl.current.focus();
+
+          setQuery("");
+
+        }
+
+      }
+
+
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.addEventListener("keydown", callback);
+
+    },
+
+    [setQuery]
+
+  );
+
+
+
+  // useEffect(function () {
+
+  //   const el = document.querySelector(".search");
+
+  //   console.log(el);
+
+  //   el.focus();
+
+  // }, []);
   
   return (
         <input
@@ -230,6 +244,7 @@ function Search(query, setQuery) {
           placeholder="Search movies..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
+          ref={inputEl}
         />
   );
 }
